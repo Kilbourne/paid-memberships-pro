3141b83d0d5f9a56ce1dfa76c960827ca98d971d
David Parker <dlparker1005@gmail.com>
2023-07-12 13:33:40 -0400

    Merge pull request #2493 from JarrydLong/getoption-classes
    getOptionRenaming - classes folder
    # Conflicts:
    #	classes/class.pmproemail.php
    #	classes/gateways/class.pmprogateway_check.php
    #	classes/gateways/class.pmprogateway_stripe.php

--- classes/class-pmpro-site-health.php
+++ classes/class-pmpro-site-health.php
@@ -119,7 +119,7 @@ class PMPro_Site_Health {
 				],
 				'pmpro-recorded-site-url' => [
 					'label' => __( 'Last Known Site URL', 'paid-memberships-pro' ),
-					'value' => pmpro_getOption( 'last_known_url' ),
+					'value' => get_option( 'pmpro_last_known_url' ),
 				],
 				'pmpro-pause-mode' => [
 					'label' => __( 'Pause Mode', 'paid-memberships-pro' ),
@@ -611,7 +611,7 @@ class PMPro_Site_Health {
 			],
 		];
 
-		$gateway = pmpro_getOption( 'gateway' );
+		$gateway = get_option( 'pmpro_gateway' );
 
 		if ( $gateway && isset( $gateway_specific_constants[ $gateway ] ) ) {
 			$constants = array_merge( $constants, $gateway_specific_constants[ $gateway ] );
